5a40211296d26e1514ddf0320d4e1773a69ffa2d
ashvin.bhuttoo <ashvin.bhuttoo@gmail.com>
2019-11-19 14:17:49 +0400

    fix random crash with error code 0xc0000005

--- Runner/Runner.cpp
+++ Runner/Runner.cpp
@@ -12,11 +12,12 @@ HANDLE MapFileToMemory(LPCSTR filename);
 
 int main()
 {
-	//RunPortableExecutable(MapFileToMemory("..\\Debug\\Stub.exe"), "from_file");
+	RunPortableExecutable(MapFileToMemory("..\\Debug\\Stub.exe"), "from_file");
 	RunPortableExecutable((void*)rawData, "from_memory");
 
 	cout << "Runner Thread." << endl;
-	getchar();
+	while(true)
+		getchar();
 }
 
 // use this if you want to read the executable from disk
@@ -41,67 +42,54 @@ HANDLE MapFileToMemory(LPCSTR filename)
 
 int RunPortableExecutable(void* Image, string cmdLine)
 {
-	IMAGE_DOS_HEADER* DOSHeader; // For Nt DOS Header symbols
-	IMAGE_NT_HEADERS* NtHeader; // For Nt PE Header objects & symbols
+	IMAGE_DOS_HEADER* DOSHeader;
+	IMAGE_NT_HEADERS* NtHeader;
 	IMAGE_SECTION_HEADER* SectionHeader;
-
 	PROCESS_INFORMATION PI;
 	STARTUPINFOA SI;
-
 	CONTEXT* CTX;
-
-	DWORD* ImageBase; //Base address of the image
-	void* pImageBase; // Pointer to the image base
-
+	DWORD* ImageBase = NULL;
+	void* pImageBase = NULL;
 	int count;
 	char CurrentFilePath[1024];
-
-	DOSHeader = PIMAGE_DOS_HEADER(Image); // Initialize Variable
-	NtHeader = PIMAGE_NT_HEADERS(DWORD(Image) + DOSHeader->e_lfanew); // Initialize
-
-	GetModuleFileNameA(0, CurrentFilePath, 1024); // path to current executable
-
-	if (NtHeader->Signature == IMAGE_NT_SIGNATURE) // Check if image is a PE File.
+	DOSHeader = PIMAGE_DOS_HEADER(Image);
+	NtHeader = PIMAGE_NT_HEADERS(DWORD(Image) + DOSHeader->e_lfanew);
+	GetModuleFileNameA(0, CurrentFilePath, 1024);
+	if (NtHeader->Signature == IMAGE_NT_SIGNATURE)
 	{
-		ZeroMemory(&PI, sizeof(PI)); // Null the memory
-		ZeroMemory(&SI, sizeof(SI)); // Null the memory
-
+		ZeroMemory(&PI, sizeof(PI));
+		ZeroMemory(&SI, sizeof(SI));
 		cmdLine = "Stub.exe " + cmdLine;
-		if (CreateProcessA(CurrentFilePath, (LPSTR)cmdLine.c_str(), NULL, NULL, FALSE,
-			CREATE_SUSPENDED, NULL, NULL, &SI, &PI)) // Create a new instance of current
-			//process in suspended state, for the new image.
+		bool threadcreated = CreateProcessA(CurrentFilePath, (LPSTR)cmdLine.c_str(), NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &SI, &PI);
+		if (threadcreated == true)
 		{
-			// Allocate memory for the context.
 			CTX = LPCONTEXT(VirtualAlloc(NULL, sizeof(CTX), MEM_COMMIT, PAGE_READWRITE));
-			CTX->ContextFlags = CONTEXT_FULL; // Context is allocated
-
-			if (GetThreadContext(PI.hThread, LPCONTEXT(CTX))) //if context is in thread
+			CTX->ContextFlags = CONTEXT_FULL;
+			if (GetThreadContext(PI.hThread, LPCONTEXT(CTX)))
 			{
-				// Read instructions
 				ReadProcessMemory(PI.hProcess, LPCVOID(CTX->Ebx + 8), LPVOID(&ImageBase), 4, 0);
-
 				pImageBase = VirtualAllocEx(PI.hProcess, LPVOID(NtHeader->OptionalHeader.ImageBase),
 					NtHeader->OptionalHeader.SizeOfImage, 0x3000, PAGE_EXECUTE_READWRITE);
-
-				// Write the image to the process
-				WriteProcessMemory(PI.hProcess, pImageBase, Image, NtHeader->OptionalHeader.SizeOfHeaders, NULL);
-
-				for (count = 0; count < NtHeader->FileHeader.NumberOfSections; count++)
-				{
-					SectionHeader = PIMAGE_SECTION_HEADER(DWORD(Image) + DOSHeader->e_lfanew + 248 + (count * 40));
-
-					WriteProcessMemory(PI.hProcess, LPVOID(DWORD(pImageBase) + SectionHeader->VirtualAddress),
-						LPVOID(DWORD(Image) + SectionHeader->PointerToRawData), SectionHeader->SizeOfRawData, 0);
+				if (pImageBase == 00000000) {
+					ResumeThread(PI.hThread);
+					ExitProcess(NULL);
+					return 1;
 				}
-				WriteProcessMemory(PI.hProcess, LPVOID(CTX->Ebx + 8),
-					LPVOID(&NtHeader->OptionalHeader.ImageBase), 4, 0);
-
-				// Move address of entry point to the eax register
-				CTX->Eax = DWORD(pImageBase) + NtHeader->OptionalHeader.AddressOfEntryPoint;
-				SetThreadContext(PI.hThread, LPCONTEXT(CTX)); // Set the context
-				ResumeThread(PI.hThread); //´Start the process/call main()
-
-				return 0; // Operation was successful.
+				if (pImageBase > 0) {
+					WriteProcessMemory(PI.hProcess, pImageBase, Image, NtHeader->OptionalHeader.SizeOfHeaders, NULL);
+					for (count = 0; count < NtHeader->FileHeader.NumberOfSections; count++)
+					{
+						SectionHeader = PIMAGE_SECTION_HEADER(DWORD(Image) + DOSHeader->e_lfanew + 248 + (count * 40));
+						WriteProcessMemory(PI.hProcess, LPVOID(DWORD(pImageBase) + SectionHeader->VirtualAddress),
+							LPVOID(DWORD(Image) + SectionHeader->PointerToRawData), SectionHeader->SizeOfRawData, 0);
+					}
+					WriteProcessMemory(PI.hProcess, LPVOID(CTX->Ebx + 8),
+						LPVOID(&NtHeader->OptionalHeader.ImageBase), 4, 0);
+					CTX->Eax = DWORD(pImageBase) + NtHeader->OptionalHeader.AddressOfEntryPoint;
+					SetThreadContext(PI.hThread, LPCONTEXT(CTX));
+					ResumeThread(PI.hThread);
+					return 0;
+				}
 			}
 		}
 	}
